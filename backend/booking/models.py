a9902649c422d74b93bef89860fe50ebad60c0c0
AIDev <developer328555@gmail.com>
2022-10-13 16:23:30 +0400

    create base models for booking and create profile for linking def_currency for user

--- backend/booking/models.py
+++ backend/booking/models.py
@@ -1,3 +1,4 @@
+from django.utils.timezone import now
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -20,3 +21,21 @@ class Account(models.Model):
 class Transaction(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    calculated = models.BooleanField(default=False)
+    datetime = models.DateTimeField(default=now)
+
+
+class Convertation(models.Model):
+    from_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='+')
+    to_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='+')
+    source_transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='source_in_convertation')
+    target_transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='target_in_convertation')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_in = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Cost(models.Model):
+    transacton = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    source_transacton = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='source_in_cost')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_in_default_currency = models.DecimalField(max_digits=10, decimal_places=2)
---
+++ backend/user_profile/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from booking.models import Currency
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if Currency.objects.count() > 0 and instance.default_currency is None:
+            Profile.objects.create(user=instance, default_currency = Currency.objects.first())
+            return
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
